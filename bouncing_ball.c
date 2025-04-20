6e1616875dd62ab2e9bf68e0701dca2966d3a7b8
Roverp <yar.zubaha@proton.me>
2025-04-20 22:40:37 +0200

    feat: add setup_animation func & add SpriteDirection enum

--- bouncing_ball.c
+++ bouncing_ball.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,24 +12,47 @@
 #define HIDE_CURSOR "\033[?25l"
 #define SHOW_CURSOR "\033[?25h"
 
+typedef enum {
+  DIRECTION_UP_LEFT,
+  DIRECTION_UP_RIGHT,
+  DIRECTION_DOWN_LEFT,
+  DIRECTION_DOWN_RIGHT
+} SpriteDirection;
+
 typedef struct {
-  short direction_x, direction_y;
+  SpriteDirection direction;
   unsigned short position_column, position_row;
-} SoccerBallPositioning;
+} SpritePositioning;
 
 typedef struct {
   char lines[MAX_ARR_LENGHT][MAX_STRING_LENGTH];
   unsigned short width, height;
 } Sprite;
 
-const Sprite load_sprite_from_file(const char *file_path);
-const unsigned short load_sprite_lines(char linesArr[][MAX_STRING_LENGTH],
-                                       FILE *opened_file,
-                                       unsigned short *max_width);
+typedef struct {
+  short dy, dx;
+} MovementVector;
 
-int main() { return 0; }
+Sprite load_sprite_from_file(const char *file_path);
+unsigned short load_sprite_lines(char linesArr[][MAX_STRING_LENGTH],
+                                 FILE *opened_file, unsigned short *max_width);
+void perform_terminal_setup_for_animation();
+void restore_terminal();
+MovementVector get_movement_vector(SpriteDirection sprite_direction);
+SpriteDirection flip_horizontal_direction(SpriteDirection direction);
+SpriteDirection flip_vertical_direction(SpriteDirection direction);
 
-const Sprite load_sprite_from_file(const char *file_path) {
+int main() {
+  struct winsize terminal_size;
+
+  Sprite soccerBallSprite = load_sprite_from_file("./assets/soccer_ball.txt");
+
+  perform_terminal_setup_for_animation();
+
+  return 0;
+}
+
+Sprite load_sprite_from_file(const char *file_path) {
   Sprite soccerBallSprite = {
       .width = 0,
       .height = 0,
@@ -47,9 +71,8 @@ const Sprite load_sprite_from_file(const char *file_path) {
   return soccerBallSprite;
 }
 
-const unsigned short load_sprite_lines(char linesArr[][MAX_STRING_LENGTH],
-                                       FILE *opened_file,
-                                       unsigned short *max_width) {
+unsigned short load_sprite_lines(char linesArr[][MAX_STRING_LENGTH],
+                                 FILE *opened_file, unsigned short *max_width) {
 
   unsigned short line_index = 0;
 
@@ -67,4 +90,56 @@ const unsigned short load_sprite_lines(char linesArr[][MAX_STRING_LENGTH],
   }
 
   return line_index;
+}
+
+void perform_terminal_setup_for_animation() {
+  printf(HIDE_CURSOR);
+  atexit(restore_terminal);
+}
+
+void restore_terminal() { printf(SHOW_CURSOR); }
+
+MovementVector get_movement_vector(SpriteDirection sprite_direction) {
+  switch (sprite_direction) {
+  case DIRECTION_UP_LEFT:
+    return (MovementVector){-1, -1};
+  case DIRECTION_UP_RIGHT:
+    return (MovementVector){-1, 1};
+  case DIRECTION_DOWN_LEFT:
+    return (MovementVector){1, -1};
+  case DIRECTION_DOWN_RIGHT:
+    return (MovementVector){1, 1};
+  default:
+    assert(0 && "Ureachable: invalid SpriteDirection");
+  }
+}
+
+SpriteDirection flip_horizontal_direction(SpriteDirection direction) {
+  switch (direction) {
+  case DIRECTION_UP_LEFT:
+    return DIRECTION_UP_RIGHT;
+  case DIRECTION_UP_RIGHT:
+    return DIRECTION_UP_LEFT;
+  case DIRECTION_DOWN_LEFT:
+    return DIRECTION_DOWN_RIGHT;
+  case DIRECTION_DOWN_RIGHT:
+    return DIRECTION_DOWN_LEFT;
+  default:
+    assert(0 && "Ureachable: invalid SpriteDirection");
+  }
+}
+
+SpriteDirection flip_vertical_direction(SpriteDirection direction) {
+  switch (direction) {
+  case DIRECTION_UP_LEFT:
+    return DIRECTION_DOWN_LEFT;
+  case DIRECTION_DOWN_LEFT:
+    return DIRECTION_UP_LEFT;
+  case DIRECTION_UP_RIGHT:
+    return DIRECTION_DOWN_RIGHT;
+  case DIRECTION_DOWN_RIGHT:
+    return DIRECTION_UP_RIGHT;
+  default:
+    assert(0 && "Ureachable: invalid SpriteDirection");
+  }
 }
